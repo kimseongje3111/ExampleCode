a4046bba01f2d2d2f09871ed1636553478bc8f2a
Seongje KIm <tjdwp3s@gmail.com>
2020-04-10 20:59:11 +0900

    [spring-boot-example] jpashop : Item domain implement

--- Spring/spring-boot-example/japshop/src/main/java/jpabook/japshop/domain/item/Item.java
+++ Spring/spring-boot-example/japshop/src/main/java/jpabook/japshop/domain/item/Item.java
@@ -1,6 +1,7 @@
 package jpabook.japshop.domain.item;
 
 import jpabook.japshop.domain.Category;
+import jpabook.japshop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -26,4 +27,17 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    // == 비지니스 로직 == //
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+
+        if (restStock < 0 ) throw new NotEnoughStockException("재고가 더 필요합니다.");
+
+        this.stockQuantity -= quantity;
+    }
 }
---
+++ Spring/spring-boot-example/japshop/src/main/java/jpabook/japshop/service/ItemService.java
@@ -0,0 +1,36 @@
+package jpabook.japshop.service;
+
+import jpabook.japshop.domain.item.Item;
+import jpabook.japshop.repository.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class ItemService {
+
+    private final ItemRepository repository;
+
+    /**
+     * 상품 등록
+     */
+    @Transactional
+    public void saveItem(Item item) {
+        repository.save(item);
+    }
+
+    /**
+     * 상품 조회
+     */
+    public List<Item> findItems() {
+        return repository.findAll();
+    }
+
+    public Item findOne(Long id) {
+        return repository.findOne(id);
+    }
+}
