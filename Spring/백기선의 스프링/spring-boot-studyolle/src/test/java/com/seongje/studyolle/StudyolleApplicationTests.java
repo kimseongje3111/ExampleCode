9e4c5a83ba97adf6316c6449300ee892a1cef17e
Seongje KIm <tjdwp3s@gmail.com>
2020-05-13 17:44:12 +0900

    Chanage directory name and add new project for studyolle

---
+++ Spring/백기선의 스프링/spring-boot-studyolle/src/test/java/com/seongje/studyolle/StudyolleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seongje.studyolle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyolleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
